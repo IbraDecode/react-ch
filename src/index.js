ec4854949d6a1f6985d727c878ed482d116af32a
Ibra Decode <ibradecode@gmail.com>
2026-02-22 10:38:28 +0800

    feat: support urlChannel

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ module.exports = async (req, res) => {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const { newsletterId, messageId, emojiReact } = req.body;
+  const { newsletterId, messageId, emojiReact, urlChannel } = req.body;
 
   if (!newsletterId || !messageId) {
     return res.status(400).json({ 
@@ -33,6 +33,7 @@ module.exports = async (req, res) => {
   console.log('Time:', new Date().toISOString());
   console.log('Newsletter ID:', newsletterId);
   console.log('Message ID:', messageId);
+  console.log('URL Channel:', urlChannel || '-');
   console.log('Emoji:', emoji);
   console.log('========================================');
 
@@ -43,6 +44,7 @@ module.exports = async (req, res) => {
     received: {
       newsletterId,
       messageId,
+      urlChannel,
       time: new Date().toISOString()
     }
   });
